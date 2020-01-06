eaca460b85727ba87361875eb3d2b932af84846b
bilbravo <bilbravo@gmail.com>
2020-01-05 20:41:33 -0500

    updated application

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = (Button) findViewById(R.id.ask_button);
+        final ImageView eightBallImage = (ImageView) findViewById(R.id.eight_ball);
+
+        final int[] eightBallArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+                Random randomNumberGenerator = new Random();
+                int pickAnswer = randomNumberGenerator.nextInt(5);
+                eightBallImage.setImageResource(eightBallArray[pickAnswer]);
+
+            }
+        });
     }
 }
